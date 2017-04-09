4a427a7d712b25cb5f5d1d379e9989fe80a5b91a
Thineshth <th.thinesh@gmail.com>
2017-04-09 13:32:30 -0400

    Added views to Article model

--- ByteSizeNews/models.py
+++ ByteSizeNews/models.py
@@ -17,7 +17,7 @@ class Source(Document):
     sortBysAvailable = ListField(StringField())
     urlsToLogos = ListField(StringField())
 
-    def __str__ (self):
+    def __str__(self):
         return "{0}".format(self.source_id)
 
 
@@ -34,6 +34,7 @@ class Article(Document):
     ratings = ListField(ReferenceField(Rating))
     keywords = ListField(StringField())
     sentiment = FloatField()
+    views = IntField()
 
-    def __str__ (self):
+    def __str__(self):
         return "{0}:{1}".format(self.title, self.url)
