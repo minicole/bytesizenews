455669d2b27fcde9be292c44b000580a26f5334f
Nicole Cappadocia-Assaly <nicolecappadocia@gmail.com>
2017-04-10 14:02:04 -0400

    Fixed support for active tab.

---
+++ bytesizenewsng/app/bs-active.js
@@ -0,0 +1,28 @@
+/**
+ * Created by nicol on 2017-04-10.
+ */
+
+
+angular.module('myApp')
+.directive('bsActiveLink', ['$location', function ($location) {
+return {
+    restrict: 'A', //use as attribute
+    replace: false,
+    link: function (scope, elem) {
+        //after the route has changed
+        scope.$on("$routeChangeSuccess", function () {
+            var hrefs = ['/#!' + $location.path(),
+                         '#!' + $location.path(), //html5: false
+                         $location.path()]; //html5: true
+            angular.forEach(elem.find('a'), function (a) {
+                a = angular.element(a);
+                if (-1 !== hrefs.indexOf(a.attr('href'))) {
+                    a.parent().addClass('active');
+                } else {
+                    a.parent().removeClass('active');
+                };
+            });
+        });
+    }
+}
+}]);
