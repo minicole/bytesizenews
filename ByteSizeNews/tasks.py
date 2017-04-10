d1428e3d715a52ca1da2e8394d18202654b85e21
Thineshth <th.thinesh@gmail.com>
2017-04-10 00:12:40 -0400

    Fixed tasks error

--- ByteSizeNews/tasks.py
+++ ByteSizeNews/tasks.py
@@ -3,16 +3,16 @@ from celery.utils.log import get_task_logger
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from ByteSizeNews.NewsFetchService import *
+from django.conf import settings
 
 
 logger = get_task_logger(__name__)
 
 # Fetch on the hour  0 minutes crontab notation
-if DEBUG:
-    NewsFetchingPeriod = '*/5'
-    SourceFetchingPeriod = '*/3'
-else:
-    NewsFetchingPeriod = '*/45'
+
+# NewsFetchingPeriod = '*/5'
+# SourceFetchingPeriod = '*/3'
+NewsFetchingPeriod = '*/45'
 
 @periodic_task(run_every=(crontab(minute=0)), name="news_fetching_task", ignore_result=True)
 def fetch_news():
